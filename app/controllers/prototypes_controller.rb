1477b3343df58dff20d10d9dfa9a083645716e6f
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-08-18 20:23:08 +0900

    編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
     before_action :authenticate_user!  ,except: [:index, :show]
     before_action :move_to_index, except: [:index, :show]
-     # before_action :set_prototype, only:[:edit,:show]
+    before_action :set_prototype, only:[:edit,:show]
   def index
     @prototypes = Prototype.all
   end 
@@ -20,13 +20,13 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
+   # @prototype = Prototype.find(params[:id])
     @comment =  Comment.new
     @comments = @prototype.comments
   end
 
   def edit 
-    @prototype = Prototype.find(params[:id])
+   # @prototype = Prototype.find(params[:id])
    end
 
   def update
@@ -46,6 +46,11 @@ class PrototypesController < ApplicationController
     end
 
   private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+    
+  end
   def prototype_params  
     params.require(:prototype).permit(:image,:title,:catch_copy,:concept, ).merge(user_id: current_user.id) 
   end
